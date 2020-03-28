146581678941144aa8a602c9d90832e7d5ec3d1d
Kirion1st <lassedu@t-online.de>
2020-03-28 01:55:50 +0100

    Updating the JavaDoc of registerWindowListener

--- src/de/kirion/game/GameWindow.java
+++ src/de/kirion/game/GameWindow.java
@@ -39,7 +39,7 @@ public class GameWindow extends JFrame{
     }
     
     /**
-     * Window Listener is registered
+     * registers a WindowListener
      */
     private void registerWindowListener() {
     	
